1e0dbe39b04e2c25414889d39f3a3bfd82e5e3e6
TheDrone7 <hsbhatia765@gmail.com>
2022-05-09 13:42:11 +0530

    Remove typedoc, add vuepress

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vuepress/config';
+
+export default defineConfig({
+  title: 'Shieldbow',
+  description: 'An all-purpose, easy-to-use API wrapper for the league of legends API.'
+});
