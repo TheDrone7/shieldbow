e125b8aefe1ed93f9f2044d6b3d26917bf5e1834
TheDrone7 <hsbhatia765@gmail.com>
2023-03-07 04:39:00 -0500

    Fix docsearch

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -11,9 +11,9 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       algolia: {
+        apiKey: '9ec0159c389f1b462ece11c3d5a70431',
+        applicationId: 'BU2EAYG78F',
         docSearch: {
-          appId: 'BU2EAYG78F',
-          apiKey: '9ec0159c389f1b462ece11c3d5a70431',
           indexName: 'shieldbow'
         }
       }
