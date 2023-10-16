21b40dc69210dd8672a77676f434ff601f37a1b5
TheDrone7 <h@thedrone7.dev>
2023-10-16 03:25:50 -0400

    Fix index.js

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -1,16 +1,5 @@
-import { Client } from './client';
+export { Client } from './client';
 export * from 'types';
 export * from 'utilities';
 export * from 'structures';
 export * from 'managers';
-
-const client = new Client();
-
-client
-  .initialize({
-    version: '13.10.1'
-  })
-  .then(async () => {
-    const kayn = await client.champions.fetch('Kayn');
-    console.log(kayn.name);
-  });
