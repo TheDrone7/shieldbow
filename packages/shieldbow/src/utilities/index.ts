44a926226dbf41807b164dc5a9cb1166b3ca5ded
TheDrone7 <h@thedrone7.dev>
2023-12-28 23:51:38 -0500

    Add base URLs

--- packages/shieldbow/src/utilities/index.ts
+++ packages/shieldbow/src/utilities/index.ts
@@ -1,3 +1,4 @@
 export { parseClientConfig, parseWebConfig } from './parseConfig';
 export { parseFetchOpts, divideToManager } from './parseFetchOpts';
 export { genWebConfig } from './genWebConfig';
+export { apiBaseURLs, regionalURLs, sea as seaRegions } from './urls';
