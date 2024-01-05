bbe02b2329dab1e53c8d6453d43da9872e64b23c
TheDrone7 <h@thedrone7.dev>
2024-01-05 05:00:45 -0500

    Add summoner DTO to types

--- packages/shieldbow/src/types/api/index.ts
+++ packages/shieldbow/src/types/api/index.ts
@@ -1,1 +1,2 @@
 export type { IAccount } from './account';
+export type { ISummoner } from './summoner';
---
+++ packages/shieldbow/src/types/api/summoner.ts
@@ -0,0 +1,11 @@
+/**
+ * The raw summoner data received from the API.
+ */
+export interface ISummoner {
+  profileIconId: number;
+  revisionDate: number;
+  name: string;
+  id: string;
+  puuid: string;
+  summonerLevel: number;
+}
