c4aa12275c88c855c070a278678be9b87d895f8a
TheDrone7 <h@thedrone7.dev>
2024-01-06 04:08:15 -0500

    Add champion mastery type

--- packages/shieldbow/src/types/api/index.ts
+++ packages/shieldbow/src/types/api/index.ts
@@ -1,3 +1,4 @@
 export type { IAccount } from './account';
 export type { ISummoner } from './summoner';
 export type { IChampionRotation } from './champion';
+export type { IChampionMastery } from './mastery';
